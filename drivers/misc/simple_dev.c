4c6b96c432343b0c05baa4f7f78fdb9569281952
VladPodilnyk <vlad.podilnyk@gmail.com>
2017-11-09 11:51:35 +0200

    Add kernel module with timer
    Last version of the module.
    Add timer to check how long
    the module is inserted.

--- drivers/misc/simple_dev.c
+++ drivers/misc/simple_dev.c
@@ -4,15 +4,22 @@
 #include <linux/errno.h>
 #include <linux/slab.h>
 #include <linux/string.h>
+#include <linux/time.h>
 
 char* wait_msg;
+struct
+{
+  struct timeval start;
+  struct timeval finish;
+} timer;
 
 
 static int __init mk_life_init(void)
 {
-	wait_msg = NULL;//(char*)kmalloc(7 * sizeof(char), GFP_KERNEL);
-	//if (!wait_msg)
-	//	return -ENOMEM;
+	do_gettimeofday(& timer.start);
+	wait_msg = (char*)kmalloc(7 * sizeof(char), GFP_KERNEL);
+	if (!wait_msg)
+		return -ENOMEM;
 	memcpy(wait_msg, "Wait...", 7);
         printk("Making your life simplier\n");
 	printk("%s\n", wait_msg);
@@ -22,9 +29,10 @@ static int __init mk_life_init(void)
 
 static void __exit mk_life_exit(void)
 {	
-	printk("%s\n", wait_msg);
-	kfree(wait_msg);
-	printk("Congrats!!! Your life is simple\n");
+	kfree(wait_msg);;
+	do_gettimeofday(& timer.finish);
+	printk("Work time: %ld sec.\n" timer.finish.tv_sec - timer.start.tv_sec);
+        printk("Congrats!!! Your life is simple\n");
 	
 }
 
