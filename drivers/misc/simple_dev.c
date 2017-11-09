824b381d66437b2a244f983da73ff60d3591d209
VladPodilnyk <vlad.podilnyk@gmail.com>
2017-11-09 10:34:24 +0200

    Add my kernel module
    In this first version I try to return
    positive sign of an error from init
    function of the module. Now when you
    perform insmod /lib/simple_dev.ko,
    you should see something like this
    do_init_module:'simple_dev'->init
    suspiciously.

---
+++ drivers/misc/simple_dev.c
@@ -0,0 +1,38 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+
+char* wait_msg;
+
+
+static int __init mk_life_init(void)
+{
+	wait_msg = (char*)kmalloc(7 * sizeof(char), GFP_KERNEL);
+	//if (!wait_msg)
+	//	return ENOMEM;
+	memcpy(wait_msg, "Wait...", 7);
+        printk("Making your life simplier\n");
+	printk("%s\n", wait_msg);
+	return ENOMEM;
+}
+
+
+static void __exit mk_life_exit(void)
+{	
+	printk("%s\n", wait_msg);
+	kfree(wait_msg);
+	printk("Congrats!!! Your life is simple\n");
+	
+}
+
+
+module_init(mk_life_init);
+module_exit(mk_life_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple device module");
+MODULE_AUTHOR("Vladyslav Podilnyk");
+
