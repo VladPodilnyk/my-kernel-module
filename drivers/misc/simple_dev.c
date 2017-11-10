570c96bf110d73c5f51b9d6277c15aaac3db9878
VladPodilnyk <vlad.podilnyk@gmail.com>
2017-11-10 10:20:34 +0200

    Add debugfs to the module
    Now you can obtain information
    about numbers of interrupts that fired
    using cat /sys/kernel/simple-dev/uart-pl011-count

--- drivers/misc/simple_dev.c
+++ drivers/misc/simple_dev.c
@@ -8,45 +8,101 @@
 #include <linux/irqreturn.h>
 #include <linux/interrupt.h>
 #include <linux/serial_core.h>
+#include <linux/debugfs.h>
+#include <linux/spinlock.h>
 
 #define SIMPLE_DEV_IRQ 53
 
 static struct uart_port *uap;
 
+static struct debugfs_data
+{
+  struct dentry *dir;
+  struct dentry *file;
+  u32   	counter;
+  spinlock_t 	lock;
+} *debugfs;
+
 static irqreturn_t simple_irq_handler_top(int irq, void *dev_id)
 {
-	printk("Top: %i\n", current->pid);
-	return IRQ_WAKE_THREAD;
-}
-
-static irqreturn_t simple_irq_handler_bottom(int irq, void *dev_id)
-{
-	msleep(200);
-	printk("Bottom: %i\n", current->pid);
+	unsigned long flags;
+	spin_lock_irqsave(&debugfs->lock, flags);
+	++debugfs->counter;
+	spin_unlock_irqrestore(&debugfs->lock, flags);
 	return IRQ_HANDLED;
-
 }
 
 
 static int __init mk_life_init(void)
 {
-	int irq_ret;
-
+	int ret;
+	
+	debugfs = kmalloc(sizeof(struct debugfs_data), GFP_KERNEL);
 	uap = kmalloc(sizeof(struct uart_port), GFP_KERNEL);
-	if (!uap)
-		return -ENOMEM;
-	irq_ret = request_threaded_irq(SIMPLE_DEV_IRQ, simple_irq_handler_top, simple_irq_handler_bottom, IRQF_SHARED, "simple-dev", uap);
-	if (irq_ret < 0)
-		return -ENXIO;
+	if (!debugfs || !uap) {
+		ret = -ENOMEM;
+		goto ret_fail_alloc_mem;
+	}
+	
+	spin_lock_init(&debugfs->lock);
+	debugfs->counter = 0;
+
+	debugfs->dir = debugfs_create_dir("simple-dev", NULL);
+	if (!debugfs->dir) {
+		ret = -ENOENT;
+		goto ret_no_such_dir;
+	}
+
+	if ( (int)debugfs->dir == -ENODEV) {
+        	ret = -ENODEV;
+        	goto ret_no_such_dir;
+    	}
+	
+	debugfs->file = debugfs_create_u32("uart-pl011-count", S_IRUSR | S_IRWXU, debugfs->dir, &debugfs->counter);
+	if (!debugfs->file) {
+		ret = -ENOENT;
+		goto ret_no_such_file;
+	}
+
+	if ( (int)debugfs->file == -ENODEV) {
+        	ret = -ENODEV;
+        	goto ret_no_such_file;
+    	}
+
+
+	ret = request_irq(SIMPLE_DEV_IRQ, simple_irq_handler_top, IRQF_SHARED, "simple-dev", uap);
+	if (ret < 0) {
+		ret = -ENXIO;
+		goto ret_no_such_dev;
+	}
+	
 	printk("Making your life simplier...\n");
 	return 0;
+
+ret_fail_alloc_mem:
+	return ret;
+
+ret_no_such_dir:
+	kfree(debugfs);
+	return ret;
+
+ret_no_such_file:
+	debugfs_remove_recursive(debugfs->dir);
+	kfree(debugfs);
+	return ret;
+
+ret_no_such_dev:
+	debugfs_remove_recursive(debugfs->dir);
+	kfree(debugfs);
+	return ret;
 }
 
 
 static void __exit mk_life_exit(void)
 {	
 	free_irq(SIMPLE_DEV_IRQ, uap);
-	kfree(uap);
+	debugfs_remove_recursive(debugfs->dir);
+	kzfree(debugfs);
 	printk("Congrats!!! Your life is simple.\n");
 }
 
